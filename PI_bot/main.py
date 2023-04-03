d30d1ad5210cd39a5c0d877b63ce7abc0e3479eb
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-04-03 21:12:30 +0200

    added commands to handle

--- PI_bot/main.py
+++ PI_bot/main.py
@@ -1,22 +1,28 @@
-import time
 import random
-import datetime
-import telepot
 import sys
 import os
+import time
+import datetime
+import schedule
+import re
+import telepot
 from telepot.loop import MessageLoop
 
 OWNERS_ID = []
 bot = telepot.Bot('')
 
+time_pattern = "^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$"
+
 unauthorized_supported_commands = ['/roll',
                                    '/time',
-                                   '/my-id',
-                                   '/ai']
+                                   '/id']
 
-authorized_supported_commands = ['/self-log',
-                                 '/auth-log',
-                                 '/stats']
+authorized_supported_commands = ['/self',
+                                 '/auth',
+                                 '/stats',
+                                 '/stats_time',
+                                 '/stats_jobs',
+                                 '/stats_cancel']
 
 combined_commands = [authorized_supported_commands,
                      unauthorized_supported_commands]
@@ -37,53 +43,104 @@ def redirect_std_out():
     os.system(path)
 
 
-def owners_commands(user_id, chat_id, command):
+def mine_log_task(chat_id):
+    bot.sendMessage(chat_id, "TEST")
+    return
+
+
+def owners_commands(user_id, chat_id, command_type, command_mode):
     if OWNERS_ID.__contains__(user_id):
-        if command == authorized_supported_commands[0]:
-            bot.sendMessage(chat_id, "/self-log appears soon")
-        elif command == authorized_supported_commands[1]:
-            bot.sendMessage(chat_id, "/auth-log appears soon")
-        elif command == authorized_supported_commands[2]:
+        if command_type == authorized_supported_commands[0]:
+            bot.sendMessage(chat_id, "/self appears soon")
+            return "SUCCESS"
+
+        elif command_type == authorized_supported_commands[1]:
+            bot.sendMessage(chat_id, "/auth appears soon")
+            return "SUCCESS"
+
+        elif command_type == authorized_supported_commands[2]:
             bot.sendMessage(chat_id, "/stats appears soon")
+            return "SUCCESS"
+
+        elif command_type == authorized_supported_commands[3]:
+            hours_format = re.compile(time_pattern)
+            if not hours_format.match(command_mode):
+                bot.sendMessage(chat_id, "Incorrect pattern for time. Expected HH:mm but got: '{}'"
+                                .format(command_mode))
+                return "FAILED "
+
+            schedule.every().day.at(command_mode).do(mine_log_task, chat_id).tag(chat_id)
+            bot.sendMessage(chat_id, "Log notification has been set for chat id: {} everyday at: {}."
+                            .format(chat_id, command_mode))
+            bot.sendMessage(chat_id, "Remember that, set time before 23:49.")
+            return "SUCCESS"
+        elif command_type == authorized_supported_commands[4]:
+            bot.sendMessage(chat_id, "All daily task are listed down:\n{}"
+                            .format(schedule.get_jobs()))
+            return "SUCCESS"
+
+        elif command_type == authorized_supported_commands[5]:
+            schedule.clear(chat_id)
+            bot.sendMessage(chat_id, "Log notification has been canceled for chat id: {}"
+                            .format(chat_id))
+            return "SUCCESS"
+
+        else:
+            bot.sendMessage(chat_id, "Super user, unsupported command: {} ;//"
+                            .format(command_type))
+            return "FAILED "
     else:
         bot.sendMessage(chat_id, "Sorry, you are unauthorized to use that command!")
+        return "FAILED "
 
 
 def handle(msg):
+    global status
     chat_id = msg['chat']['id']
     command = msg['text']
     user_id = msg['from']['id']
 
-    print('{}   User: {}    |    Chat id: {}  |   User input command: {}'
+    split_command = command.split(" ")
+    command_type = split_command[0]
+    command_mode = ""
+
+    if len(split_command) > 1:
+        command_mode = split_command[1]
+
+    if not verify_command(command_type):
+        bot.sendMessage(chat_id, "Sorry, unsupported command: {} ;//".format(command_type))
+        status = "FAILED "
+
+    else:
+        if command_type == unauthorized_supported_commands[0]:
+            bot.sendMessage(chat_id, random.randint(1, 6))
+            status = "SUCCESS"
+
+        elif command_type == unauthorized_supported_commands[1]:
+            bot.sendMessage(chat_id, str(datetime.datetime.now()))
+            status = "SUCCESS"
+
+        elif command_type == unauthorized_supported_commands[2]:
+            bot.sendMessage(chat_id, "Your telegram id is: {}".format(user_id))
+            status = "SUCCESS"
+
+        else:
+            status = owners_commands(user_id, chat_id, command_type, command_mode)
+
+    print('{}   User: {}    |    Chat id: {}  |   Command status: {}    |   User input command: {}'
           .format(datetime.datetime.now(),
                   chat_id,
                   user_id,
+                  status,
                   command))
 
-    if not verify_command(command):
-        bot.sendMessage(chat_id, "Sorry, unsupported command: {} ;//".format(command))
 
-    else:
-        if command == unauthorized_supported_commands[0]:
-            bot.sendMessage(chat_id, random.randint(1, 6))
-
-        elif command == unauthorized_supported_commands[1]:
-            bot.sendMessage(chat_id, str(datetime.datetime.now()))
-
-        elif command == unauthorized_supported_commands[2]:
-            bot.sendMessage(chat_id, "Your telegram id is: {}".format(msg['from']['id']))
-
-        elif command == unauthorized_supported_commands[3]:
-            bot.sendMessage(chat_id, "/ai feature will appear soon...")
-
-        else:
-            owners_commands(user_id, chat_id, command)
-
-
+status = "FAILED "
 MessageLoop(bot, handle).run_as_thread()
 # ################# DO NOT DELETE THIS LINE #################
 # redirect_std_out()
 print("I am listening since {}...".format(datetime.datetime.now()))
 
 while 1:
+    schedule.run_pending()
     time.sleep(10)
