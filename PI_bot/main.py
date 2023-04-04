41990b8f73ff218a973bafc9fe515304dce0ec67
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-04-04 12:02:42 +0200

    Refactoring code

--- PI_bot/main.py
+++ PI_bot/main.py
@@ -6,6 +6,7 @@ import datetime
 import schedule
 import re
 import telepot
+from enum import Enum
 from telepot.loop import MessageLoop
 
 OWNERS_ID = []
@@ -13,34 +14,51 @@ bot = telepot.Bot('')
 
 time_pattern = "^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$"
 path_to_bot_log = ""
-unauthorized_supported_commands = ['/roll',
-                                   '/time',
-                                   '/id']
-
-authorized_supported_commands = ['/self',
-                                 '/auth',
-                                 '/stats',
-                                 '/stats_time',
-                                 '/stats_jobs',
-                                 '/stats_cancel']
-
-combined_commands = [authorized_supported_commands,
-                     unauthorized_supported_commands]
+output_path = ""
 
 
-def verify_command(command):
-    if combined_commands[0].__contains__(command):
-        return True
-    elif combined_commands[1].__contains__(command):
-        return True
-    else:
-        return False
+class Unauthorized(Enum):
+    ROLL = '/roll'
+    TIME = '/time'
+    ID = '/id'
+
+
+class Authorized(Enum):
+    SELF = '/self'
+    AUTH = '/auth'
+    STATS = '/stats'
+    STATS_TIME = '/stats_time'
+    STATS_JOBS = '/stats_jobs'
+    STATS_CANCEL = '/stats_cancel'
+
+
+# unauthorized_supported_commands = ['/roll',
+#                                    '/time',
+#                                    '/id']
+
+# authorized_supported_commands = ['/self',
+#                                  '/auth',
+#                                  '/stats',
+#                                  '/stats_time',
+#                                  '/stats_jobs',
+#                                  '/stats_cancel']
+
+# combined_commands = [authorized_supported_commands,
+#                      unauthorized_supported_commands]
+
+
+# def verify_command(command):
+# if combined_commands[0].__contains__(command):
+#     return True
+# elif combined_commands[1].__contains__(command):
+#     return True
+# else:
+#     return False
 
 
 def redirect_std_out():
-    path = ''
     sys.stdout.flush()
-    os.system(path)
+    os.system(output_path)
 
 
 def mine_log_task(chat_id):
@@ -50,7 +68,7 @@ def mine_log_task(chat_id):
 
 def owners_commands(user_id, chat_id, command_type, command_mode):
     if OWNERS_ID.__contains__(user_id):
-        if command_type == authorized_supported_commands[0]:
+        if command_type == Authorized.SELF:
             file_data = ""
             bot.sendMessage(chat_id, "Reading file...")
             with open(path_to_bot_log, encoding='utf8') as f:
@@ -63,15 +81,15 @@ def owners_commands(user_id, chat_id, command_type, command_mode):
             bot.sendMessage(chat_id, file_data)
             return "SUCCESS"
 
-        elif command_type == authorized_supported_commands[1]:
+        elif command_type == Authorized.AUTH:
             bot.sendMessage(chat_id, "/auth appears soon")
             return "SUCCESS"
 
-        elif command_type == authorized_supported_commands[2]:
+        elif command_type == Authorized.STATS:
             bot.sendMessage(chat_id, "/stats appears soon")
             return "SUCCESS"
 
-        elif command_type == authorized_supported_commands[3]:
+        elif command_type == Authorized.STATS_TIME:
             hours_format = re.compile(time_pattern)
             if not hours_format.match(command_mode):
                 bot.sendMessage(chat_id, "Incorrect pattern for time. Expected HH:mm but got: '{}'"
@@ -83,12 +101,12 @@ def owners_commands(user_id, chat_id, command_type, command_mode):
                             .format(chat_id, command_mode))
             bot.sendMessage(chat_id, "Remember that, set time before 23:49.")
             return "SUCCESS"
-        elif command_type == authorized_supported_commands[4]:
+        elif command_type == Authorized.STATS_JOBS:
             bot.sendMessage(chat_id, "All daily task are listed down:\n{}"
                             .format(schedule.get_jobs()))
             return "SUCCESS"
 
-        elif command_type == authorized_supported_commands[5]:
+        elif command_type == Authorized.STATS_CANCEL:
             schedule.clear(chat_id)
             bot.sendMessage(chat_id, "Log notification has been canceled for chat id: {}"
                             .format(chat_id))
@@ -103,6 +121,24 @@ def owners_commands(user_id, chat_id, command_type, command_mode):
         return "FAILED "
 
 
+def normal_commands(user_id, chat_id, command_type):
+    if command_type == Unauthorized.ROLL:
+        bot.sendMessage(chat_id, random.randint(1, 6))
+        return "SUCCESS"
+
+    elif command_type == Unauthorized.TIME:
+        bot.sendMessage(chat_id, str(datetime.datetime.now()))
+        return "SUCCESS"
+
+    elif command_type == Unauthorized.ID:
+        bot.sendMessage(chat_id, "Your telegram id is: {}".format(user_id))
+        return "SUCCESS"
+
+    else:
+        bot.sendMessage(chat_id, "Sorry, unsupported command: {} ;//".format(command_type))
+        return "FAILED "
+
+
 def handle(msg):
     global status
     chat_id = msg['chat']['id']
@@ -113,30 +149,15 @@ def handle(msg):
     command_type = split_command[0]
     command_mode = ""
 
-    if len(split_command) > 1:
-        command_mode = split_command[1]
-
-    if not verify_command(command_type):
-        bot.sendMessage(chat_id, "Sorry, unsupported command: {} ;//".format(command_type))
-        status = "FAILED "
+    if OWNERS_ID.__contains__(user_id):
+        if len(split_command) > 1:
+            command_mode = split_command[1]
+        status = owners_commands(user_id, chat_id, command_type, command_mode)
 
     else:
-        if command_type == unauthorized_supported_commands[0]:
-            bot.sendMessage(chat_id, random.randint(1, 6))
-            status = "SUCCESS"
+        status = normal_commands(user_id, chat_id, command_type)
 
-        elif command_type == unauthorized_supported_commands[1]:
-            bot.sendMessage(chat_id, str(datetime.datetime.now()))
-            status = "SUCCESS"
-
-        elif command_type == unauthorized_supported_commands[2]:
-            bot.sendMessage(chat_id, "Your telegram id is: {}".format(user_id))
-            status = "SUCCESS"
-
-        else:
-            status = owners_commands(user_id, chat_id, command_type, command_mode)
-
-    print('{}   User: {}    |    Chat id: {}  |   Command status: {}    |   User input command: {}'
+    print(' {},       {},      {},     {},     {} '
           .format(datetime.datetime.now(),
                   chat_id,
                   user_id,
@@ -149,6 +170,7 @@ MessageLoop(bot, handle).run_as_thread()
 # ################# DO NOT DELETE THIS LINE #################
 # redirect_std_out()
 print("I am listening since {}...".format(datetime.datetime.now()))
+print(' Date,       User,      Chat id,     Status,     Command ')
 
 while 1:
     schedule.run_pending()
