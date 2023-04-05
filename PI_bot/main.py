2942d828a72bc6d281253d1465783293cb28850d
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-04-05 21:36:51 +0200

    Added mining log text fun

--- PI_bot/main.py
+++ PI_bot/main.py
@@ -15,6 +15,15 @@ bot = telepot.Bot('')
 time_pattern = "^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$"
 path_to_bot_log = ""
 output_path = ""
+path_to_log = ""
+
+file_sshd_service_data = []
+
+users = []
+
+users_correct_logins_in = {users[0]: []}
+users_failed_logins_tries = {users[0]: []}
+other_invalid_tries = []
 
 
 class Unauthorized(Enum):
@@ -37,9 +46,100 @@ def redirect_std_out():
     os.system(output_path)
 
 
+class Log:
+    # processing date from first fragment of log
+    @staticmethod
+    def get_date(date_line):
+        return date_line.replace(" pi ", "")
+
+    # processing ip data from second fragment of log
+    @staticmethod
+    def get_ip(ip_data_line):
+        ip_with_port_fragment = ip_data_line.split(" from ")[1]
+        ip_data = ip_with_port_fragment.split(" port ")
+        return "{}:{}".format(ip_data[0], ip_data[1])
+
+    @staticmethod
+    def send_log(message_to_sent, chat_id):
+        buffered_string = ""
+
+        for line_to_build in message_to_sent:
+            buffered_string += "{}\n".format(line_to_build)
+
+            # 3800 is needed to check because max length bot message is 4096 so 3800 is quite safe
+            if len(buffered_string) > 3800:
+                bot.sendMessage(chat_id, buffered_string)
+                buffered_string = ""
+        bot.sendMessage(chat_id, buffered_string)
+
+
 def mine_log_task(chat_id):
-    bot.sendMessage(chat_id, "TEST")
-    return
+    build_message = []
+    # reading log here
+    with open(path_to_log, encoding='utf8') as f:
+        for line in f:
+            if 'sshd' in line:
+                file_sshd_service_data.append("{}".format(line.strip()))
+
+    # mining text for each defined user
+    for user in users:
+        for line_file in file_sshd_service_data:
+
+            # splitting by "sshd" is required, because it allows to
+            # process independently date fragment and user, ip address, port fragment
+            split_line = line_file.split("sshd")
+
+            # processing fragment with user, ip address, port
+            if user in split_line[1]:
+
+                # login correct
+                if 'Accepted' in line_file:
+                    string_log = " - {} | {}".format(Log.get_date(split_line[0]), Log.get_ip(split_line[1]))
+                    users_correct_logins_in[user].append(string_log)
+
+                # login failed
+                elif 'Failed' in line_file:
+                    string_log = " - {} | {}".format(Log.get_date(split_line[0]), Log.get_ip(split_line[1]))
+                    users_failed_logins_tries[user].append(string_log)
+
+            # not existing user in SFTP server
+            elif 'Invalid' in line_file:
+                string_log = " - {} | {}".format(Log.get_date(split_line[0]), split_line[1].split(":")[1])
+                other_invalid_tries.append(string_log)
+
+        correct_count = 0
+        failed_count = 0
+        build_message.append("[{}] Log for user: {} ".format(datetime.date.today(), user))
+        build_message.append("Total correct logins")
+
+        # building correct log for user
+        for user_correct_log in users_correct_logins_in[user]:
+            correct_count += 1
+            build_message.append(user_correct_log)
+        if correct_count == 0:
+            build_message.append(" - no correct logins for user: {}".format(user))
+
+        # building incorrect log for user
+        build_message.append("Total failed logins")
+        for user_failed_log in users_failed_logins_tries[user]:
+            failed_count += 1
+            build_message.append(user_failed_log)
+        if failed_count == 0:
+            build_message.append(" - no failed logins for user: {}".format(user))
+
+        # building summary
+        build_message.append("Correct logins count: {}, Failed logins count: {}\n".format(correct_count, failed_count))
+
+    # building invalid log
+    build_message.append("\nInvalid logins to SFTP server")
+    if len(other_invalid_tries) == 0:
+        build_message.append(" - no invalid logins to server")
+    else:
+        for invalid_log in other_invalid_tries:
+            build_message.append(invalid_log)
+
+    # sending message to user
+    Log.send_log(build_message, chat_id)
 
 
 def owners_commands(user_id, chat_id, command_type, command_mode):
@@ -77,7 +177,7 @@ def owners_commands(user_id, chat_id, command_type, command_mode):
         return "SUCCESS"
 
     elif command_type == Authorized.STATS_JOBS.value:
-        bot.sendMessage(chat_id, "Function for notification log: mine_log_task(CHAT_ID)")
+        bot.sendMessage(chat_id, "Function for notification: mine_log_task(CHAT_ID)")
         bot.sendMessage(chat_id, "All daily task are listed down:\n{}".format(schedule.get_jobs()))
         return "SUCCESS"
 
@@ -108,6 +208,7 @@ def normal_commands(user_id, chat_id, command_type):
         return "FAILED "
 
 
+# every thing starts here
 def handle(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
