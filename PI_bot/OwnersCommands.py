f2f200f965d4527bdb8cb2c0183e87095546d01d
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-04-06 22:27:29 +0200

    Commands for admins and public use has been moved

---
+++ PI_bot/OwnersCommands.py
@@ -0,0 +1,78 @@
+from enum import Enum
+from PrimitiveLogger import Log
+from PrimitiveLogger import LogSender
+import re
+import schedule
+import PublicCommands
+
+
+time_pattern = "^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$"
+
+
+class AuthorizedCommands(Enum):
+    SELF = '/self'
+    AUTH = '/auth'
+    STATS = '/stats'
+    STATS_TIME = '/stats_time'
+    STATS_JOBS = '/stats_jobs'
+    STATS_CANCEL = '/stats_cancel'
+
+
+class Admin:
+
+    def __init__(self, path_to_bot_log, path_to_log, MAX_LENGTH_OF_MESSAGE, users):
+        self.path_to_bot_log = path_to_bot_log
+        self.path_to_log = path_to_log
+        self.MAX_LENGTH_OF_MESSAGE = MAX_LENGTH_OF_MESSAGE
+        self.users = users
+
+    def process_command(self, user_id, chat_id, bot, command_type, command_mode):
+        if command_type == AuthorizedCommands.SELF.value:
+
+            bot.sendMessage(chat_id, "Reading raw log file...")
+            raw_log = Log.read_raw_log(self.path_to_bot_log)
+            LogSender.send_log(raw_log, chat_id, bot, self.MAX_LENGTH_OF_MESSAGE)
+
+            return "SUCCESS"
+
+        elif command_type == AuthorizedCommands.AUTH.value:
+
+            bot.sendMessage(chat_id, "Reading raw log file...")
+            raw_log = Log.read_raw_log(self.path_to_log)
+            LogSender.send_log(raw_log, chat_id, bot, self.MAX_LENGTH_OF_MESSAGE)
+
+            return "SUCCESS"
+
+        elif command_type == AuthorizedCommands.STATS.value:
+
+            bot.sendMessage(chat_id, "Reading log file...")
+            statistics_log = Log.mine_log_task(self.path_to_log, self.users)
+            LogSender.send_log(statistics_log, chat_id, bot, self.MAX_LENGTH_OF_MESSAGE)
+
+            return "SUCCESS"
+
+        elif command_type == AuthorizedCommands.STATS_TIME.value:
+            hours_format = re.compile(time_pattern)
+            if not hours_format.match(command_mode):
+                bot.sendMessage(chat_id,
+                                "Incorrect pattern for time. Expected HH:mm but got: '{}'".format(command_mode))
+                return "FAILED "
+
+            schedule.every().day.at(command_mode).do(Log.mine_log_task, chat_id).tag(chat_id)
+
+            bot.sendMessage(chat_id, "Notification has been set everyday at: {}.".format(command_mode))
+            bot.sendMessage(chat_id, "Remember that, set time before 23:49.")
+            return "SUCCESS"
+
+        elif command_type == AuthorizedCommands.STATS_JOBS.value:
+            bot.sendMessage(chat_id, "Function for notification: Log.mine_log_task(CHAT_ID)")
+            bot.sendMessage(chat_id, "All daily task are listed down:\n{}".format(schedule.get_jobs()))
+            return "SUCCESS"
+
+        elif command_type == AuthorizedCommands.STATS_CANCEL.value:
+            schedule.clear(chat_id)
+            bot.sendMessage(chat_id, "Log notification has been canceled for chat id: {}".format(chat_id))
+            return "SUCCESS"
+
+        else:
+            return PublicCommands.public_commands(user_id, chat_id, bot, command_type)
--- PI_bot/TeleBot.py
+++ PI_bot/TeleBot.py
@@ -1,14 +1,11 @@
-import random
 import sys
 import os
 import time
 import datetime
 import schedule
-import re
 import telepot
-from PrimitiveLogger import Log
-from PrimitiveLogger import LogSender
-from enum import Enum
+import PublicCommands
+from OwnersCommands import Admin
 from telepot.loop import MessageLoop
 
 # your telegram ID
@@ -20,7 +17,6 @@ bot = telepot.Bot('')
 # i recommend that to leave this value at 3800
 MAX_LENGTH_OF_MESSAGE = 3800
 
-time_pattern = "^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$"
 
 # if you are using your bot on rasberry pi and you want to log all data from bot
 # specify output path here
@@ -31,89 +27,12 @@ path_to_log = ""
 
 # put here your name which you are using in SFTP
 users = ['pi']
-
-
-class Unauthorized(Enum):
-    ROLL = '/roll'
-    TIME = '/time'
-    ID = '/id'
-
-
-class Authorized(Enum):
-    SELF = '/self'
-    AUTH = '/auth'
-    STATS = '/stats'
-    STATS_TIME = '/stats_time'
-    STATS_JOBS = '/stats_jobs'
-    STATS_CANCEL = '/stats_cancel'
+admin = Admin(path_to_bot_log, path_to_log, MAX_LENGTH_OF_MESSAGE, users)
 
 
 def redirect_std_out():
     sys.stdout.flush()
     os.system(path_to_bot_log)
-
-
-def owners_commands(user_id, chat_id, command_type, command_mode):
-    if command_type == Authorized.SELF.value:
-        bot.sendMessage(chat_id, "Reading raw log file...")
-        raw_log = Log.read_raw_log(path_to_bot_log)
-        LogSender.send_log(raw_log, chat_id, bot, MAX_LENGTH_OF_MESSAGE)
-        return "SUCCESS"
-
-    elif command_type == Authorized.AUTH.value:
-        bot.sendMessage(chat_id, "Reading raw log file...")
-        raw_log = Log.read_raw_log(path_to_log)
-        LogSender.send_log(raw_log, chat_id, bot, MAX_LENGTH_OF_MESSAGE)
-        return "SUCCESS"
-
-    elif command_type == Authorized.STATS.value:
-        bot.sendMessage(chat_id, "Reading log file...")
-        statistics_log = Log.mine_log_task(path_to_log, users)
-        LogSender.send_log(statistics_log, chat_id, bot, MAX_LENGTH_OF_MESSAGE)
-        return "SUCCESS"
-
-    elif command_type == Authorized.STATS_TIME.value:
-        hours_format = re.compile(time_pattern)
-        if not hours_format.match(command_mode):
-            bot.sendMessage(chat_id, "Incorrect pattern for time. Expected HH:mm but got: '{}'".format(command_mode))
-            return "FAILED "
-
-        schedule.every().day.at(command_mode).do(Log.mine_log_task, chat_id).tag(chat_id)
-
-        bot.sendMessage(chat_id, "Notification has been set everyday at: {}.".format(command_mode))
-        bot.sendMessage(chat_id, "Remember that, set time before 23:49.")
-        return "SUCCESS"
-
-    elif command_type == Authorized.STATS_JOBS.value:
-        bot.sendMessage(chat_id, "Function for notification: Log.mine_log_task(CHAT_ID)")
-        bot.sendMessage(chat_id, "All daily task are listed down:\n{}".format(schedule.get_jobs()))
-        return "SUCCESS"
-
-    elif command_type == Authorized.STATS_CANCEL.value:
-        schedule.clear(chat_id)
-        bot.sendMessage(chat_id, "Log notification has been canceled for chat id: {}".format(chat_id))
-        return "SUCCESS"
-
-    else:
-        return normal_commands(user_id, chat_id, command_type)
-
-
-def normal_commands(user_id, chat_id, command_type):
-    if command_type == Unauthorized.ROLL.value:
-        bot.sendMessage(chat_id, random.randint(1, 6))
-        return "SUCCESS"
-
-    elif command_type == Unauthorized.TIME.value:
-        bot.sendMessage(chat_id, str(datetime.datetime.now()))
-        return "SUCCESS"
-
-    elif command_type == Unauthorized.ID.value:
-        bot.sendMessage(chat_id, "Your telegram id is: {}".format(user_id))
-        return "SUCCESS"
-
-    else:
-        bot.sendMessage(chat_id, "Sorry, unsupported command: {} ;//".format(command_type))
-        return "FAILED "
 
 
 # every thing starts here
@@ -131,10 +50,10 @@ def handle(msg):
 
         if len(split_command) > 1:
             command_mode = split_command[1]
-        status = owners_commands(user_id, chat_id, command_type, command_mode)
+        status = admin.process_command(user_id, chat_id, bot, command_type, command_mode)
 
     else:
-        status = normal_commands(user_id, chat_id, command_type)
+        status = PublicCommands.public_commands(user_id, chat_id, bot, command_type)
 
     print(' {},       {},      {},     {},     {} '
           .format(datetime.date.today(), chat_id, user_id, status, command))
@@ -142,9 +61,10 @@ def handle(msg):
 
 MessageLoop(bot, handle).run_as_thread()
 # ################# DO NOT DELETE THIS LINE #################
-# redirect_std_out()
+redirect_std_out()
 print("I am listening since {}...".format(datetime.date.today()))
 print('     Date,           User,          Chat id,      Status,     Command')
+
 while 1:
     schedule.run_pending()
     time.sleep(100)
